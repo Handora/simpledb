e4d82b2a4f34dba4307dcbcabe07eb27aa9ac96c
Handora <qcdsr970209@gmail.com>
2018-01-28 17:26:38 +0800

    may be buggy

--- src/java/simpledb/LockManager.java
+++ src/java/simpledb/LockManager.java
@@ -3,23 +3,30 @@ package simpledb;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.Collection;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.*;
+import java.util.concurrent.TimeUnit;
+
 
 public class LockManager {
     private Map<TransactionId, Set<PageId>> transMap;
     private Map<PageId, RWLock> pageMap;
+    private int DEFAULT_TIMEOUT = 1;
 
     public class RWLock {
         public int readCount;
         public boolean isWriteLocked;
         public boolean locked;
         public PageId pid;
+        public Lock lock;
+        public Condition condition;
 
         public RWLock(PageId pid) {
             this.readCount = 0;
             this.isWriteLocked = false;
             this.locked = false;
             this.pid = pid;
+            this.lock = new ReentrantLock();
+            this.condition = lock.newCondition();
         }
     }
 
@@ -36,7 +43,8 @@ public class LockManager {
         return transMap.get(tid);
     }
 
-    public void lockRead(TransactionId tid, PageId pid) {
+    public void lockRead(TransactionId tid, PageId pid)
+          throws TransactionAbortedException {
         // we don't need to synchronize on tid,
         // because the same tid will not happnen the same time
 
@@ -51,51 +59,59 @@ public class LockManager {
             }
         }
 
-        synchronized(l.pid) {
-            Set<PageId> trans;
-            do {
-                synchronized(tid) {
-                    trans = transMap.get(tid);
-                    if (trans == null) {
-                        trans = Collections.newSetFromMap(new ConcurrentHashMap<PageId, Boolean>());
-                        transMap.put(tid, trans);
-                    }
-                }
 
-                if (l.locked == false) {
-                    l.locked = true;
-                    l.readCount = 1;
-                    l.isWriteLocked = false;
+        l.lock.lock();
+        Set<PageId> trans;
+        do {
+            synchronized(tid) {
+                trans = transMap.get(tid);
+                if (trans == null) {
+                    trans = Collections.newSetFromMap(new ConcurrentHashMap<PageId, Boolean>());
+                    transMap.put(tid, trans);
+                }
+            }
+
+            if (l.locked == false) {
+                l.locked = true;
+                l.readCount = 1;
+                l.isWriteLocked = false;
+                synchronized(tid) {
+                    trans.add(l.pid);
+                }
+                l.lock.unlock();
+                return ;
+            }
+
+            boolean isOwned = trans.contains(l.pid);
+            if (isOwned) {
+                l.lock.unlock();
+                return ;
+            } else {
+                if (!l.isWriteLocked) {
+                    l.readCount++;
                     synchronized(tid) {
                         trans.add(l.pid);
                     }
-                    return ;
-                }
-
-                boolean isOwned = trans.contains(l.pid);
-                if (isOwned) {
+                    l.lock.unlock();
                     return ;
                 } else {
-                    if (!l.isWriteLocked) {
-                        l.readCount++;
-                        synchronized(tid) {
-                            trans.add(l.pid);
+                    try {
+                        boolean ok = l.condition.await(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+                        if (!ok) {
+                            l.lock.unlock();
+                            throw new TransactionAbortedException();
                         }
-                        return ;
-                    } else {
-                        try {
-                            l.pid.wait();
-                        } catch(InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        continue;
+                    } catch(InterruptedException e) {
+                        e.printStackTrace();
                     }
+                    continue;
                 }
-            } while (true);
-        }
+            }
+        } while (true);
     }
 
-    public void lockWrite (TransactionId tid, PageId pid) {
+    public void lockWrite (TransactionId tid, PageId pid)
+          throws TransactionAbortedException {
         RWLock l;
         synchronized(this) {
             l = pageMap.get(pid);
@@ -105,55 +121,66 @@ public class LockManager {
             }
         }
 
-        synchronized(l.pid) {
-            Set<PageId> trans;
-            do {
+
+        l.lock.lock();
+        Set<PageId> trans;
+        do {
+            synchronized(tid) {
+                trans = transMap.get(tid);
+                if (trans == null) {
+                    trans = Collections.newSetFromMap(new ConcurrentHashMap<PageId, Boolean>());
+                    transMap.put(tid, trans);
+                }
+            }
+
+            if (l.locked == false) {
+                l.locked = true;
+                l.readCount = 0;
+                l.isWriteLocked = true;
                 synchronized(tid) {
-                    trans = transMap.get(tid);
-                    if (trans == null) {
-                        trans = Collections.newSetFromMap(new ConcurrentHashMap<PageId, Boolean>());
-                        transMap.put(tid, trans);
-                    }
+                    trans.add(l.pid);
                 }
+                l.lock.unlock();
+                return ;
+            }
 
-                if (l.locked == false) {
-                    l.locked = true;
-                    l.readCount = 0;
-                    l.isWriteLocked = true;
-                    synchronized(tid) {
-                        trans.add(l.pid);
-                    }
-                    return ;
-                }
-
-                boolean isOwned = trans.contains(l.pid);
-                if (isOwned) {
-                    if (!l.isWriteLocked) {
-                        if (l.readCount == 1) {
-                            l.readCount = 0;
-                            l.isWriteLocked = true;
-                            return ;
-                        } else {
-                            try {
-                                l.pid.wait();
-                            } catch(InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            continue;
-                        }
-                    } else {
+            boolean isOwned = trans.contains(l.pid);
+            if (isOwned) {
+                if (!l.isWriteLocked) {
+                    if (l.readCount == 1) {
+                        l.readCount = 0;
+                        l.isWriteLocked = true;
+                        l.lock.unlock();
                         return ;
+                    } else {
+                        try {
+                            boolean ok = l.condition.await(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+                            if (!ok) {
+                                l.lock.unlock();
+                                throw new TransactionAbortedException();
+                            }
+                        } catch(InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        continue;
                     }
                 } else {
-                    try {
-                        l.pid.wait();
-                    } catch(InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    continue;
+                    return ;
                 }
-            } while (true);
-        }
+            } else {
+                try {
+                    boolean ok = l.condition.await(DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+                    if (!ok) {
+                        l.lock.unlock();
+                        throw new TransactionAbortedException();
+                    }
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+                continue;
+            }
+        } while (true);
+
     }
 
 
@@ -167,38 +194,38 @@ public class LockManager {
             }
         }
 
-        synchronized(l.pid) {
+        try {
+            l.lock.lock();
             Set<PageId> trans;
-            try {
-                trans = transMap.get(tid);
-                if (trans == null) {
-                    throw new DbException("unlock empty tid");
-                }
-
-                if (trans.contains(l.pid) == false) {
-                    throw new DbException("unlock none pid");
-                }
-
-                if (l.locked == false) {
-                    throw new DbException("unlock nonlocked locks");
-                }
-                synchronized(tid) {
-                    trans.remove(l.pid);
-                }
-                if (!l.isWriteLocked) {
-                    l.readCount --;
-                    if (l.readCount == 0) {
-                        l.locked = false;
-                        l.pid.notify();
-                    }
-                } else {
-                    l.locked = false;
-                    l.isWriteLocked = false;
-                    l.pid.notify();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+            trans = transMap.get(tid);
+            if (trans == null) {
+                throw new DbException("unlock empty tid");
             }
+
+            if (trans.contains(l.pid) == false) {
+                throw new DbException("unlock none pid");
+            }
+
+            if (l.locked == false) {
+                throw new DbException("unlock nonlocked locks");
+            }
+            synchronized(tid) {
+                trans.remove(l.pid);
+            }
+            if (!l.isWriteLocked) {
+                l.readCount --;
+                if (l.readCount == 0) {
+                    l.locked = false;
+                    l.condition.signal();
+                }
+            } else {
+                l.locked = false;
+                l.isWriteLocked = false;
+                l.condition.signal();
+            }
+            l.lock.unlock();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
